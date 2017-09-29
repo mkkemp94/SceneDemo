bb8bc29b2e3be1aef79dceedaca9d05c91977632
Michael Kemp <mkemp@accu-time.com>
2017-09-29 10:35:28 -0400

    Initial commit

---
+++ desktop/src/com/mygdx/scenedemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.scenedemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.scenedemo.SceneDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SceneDemo(), config);
+	}
+}
