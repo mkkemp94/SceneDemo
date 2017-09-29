11ab648ebad8ee808a5b5be37e118afcab7fb2cc
Michael Kemp <mkemp@accu-time.com>
2017-09-29 10:51:49 -0400

    Add an actor to a stage that starts moving when clicked.

--- core/src/com/mygdx/scenedemo/SceneDemo.java
+++ core/src/com/mygdx/scenedemo/SceneDemo.java
@@ -1,33 +1,83 @@
 package com.mygdx.scenedemo;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 
-public class SceneDemo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class SceneDemo implements ApplicationListener {
+
+	public class MyActor extends Actor {
+		Texture texture = new Texture("jet.png");
+		float actorX = 0, actorY = 0;
+		public boolean started = false;
+
+		public MyActor() {
+			setBounds(actorX, actorY, texture.getWidth(), texture.getHeight());
+			addListener(new InputListener() {
+				@Override
+				public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+					((MyActor) event.getTarget()).started = true;
+					return true;
+				}
+			});
+		}
+
+		@Override
+		public void draw(Batch batch, float parentAlpha) {
+			batch.draw(texture, actorX, actorY);
+		}
+
+		@Override
+		public void act(float delta) {
+			if (started) {
+				actorX += 5;
+			}
+		}
+	}
+
+	private Stage stage;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		MyActor myActor = new MyActor();
+		myActor.setTouchable(Touchable.enabled);
+		stage.addActor(myActor);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
-	
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
