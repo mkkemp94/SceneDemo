850d8164f20380fdf61cd8e04b0da55b6a3d3450
Michael Kemp <mkemp@accu-time.com>
2017-10-17 16:48:32 -0400

    All types of actions

--- core/src/com/mygdx/scenedemo/SceneWithAction.java
+++ core/src/com/mygdx/scenedemo/SceneWithAction.java
@@ -10,6 +10,13 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
 import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+import com.badlogic.gdx.utils.Pool;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.run;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+
+
+// Need this
 
 /**
  * Created by mkemp on 9/29/17.
@@ -42,11 +49,12 @@ public class SceneWithAction implements ApplicationListener {
 
         MyActor myActor = new MyActor();
 
-
+        // Actions
         MoveToAction moveAction = new MoveToAction();
         RotateToAction rotateAction = new RotateToAction();
         ScaleToAction scaleAction = new ScaleToAction();
 
+        // Define actions
         moveAction.setPosition(300f, 0f);
         moveAction.setDuration(5f);
         rotateAction.setRotation(90f);
@@ -54,18 +62,66 @@ public class SceneWithAction implements ApplicationListener {
         scaleAction.setScale(0.5f);
         scaleAction.setDuration(5f);
 
-        myActor.addAction(moveAction);
-        myActor.addAction(rotateAction);
-        myActor.addAction(scaleAction);
+        // Add actions concurrently.
+//        myActor.addAction(moveAction);
+//        myActor.addAction(rotateAction);
+//        myActor.addAction(scaleAction);
 
+        // Add actions sequentially.
 //        SequenceAction sequenceAction = new SequenceAction();
 //        sequenceAction.addAction(moveAction);
 //        sequenceAction.addAction(rotateAction);
 //        sequenceAction.addAction(scaleAction);
 //        myActor.addAction(sequenceAction);
 
-        stage.addActor(myActor);
+        // import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+
+        // Also add actions sequentially.
+//        myActor.addAction(
+//                sequence(
+//                        scaleTo(0.5f, 0.5f, 5f),
+//                        rotateTo(90.0f, 5f),
+//                        moveTo(300.0f, 0f, 5f)
+//                )
+//        );
+
+        // Also add actions in parallel
+//        myActor.addAction(
+//                parallel(
+//                        scaleTo(0.5f, 0.5f, 5f),
+//                        rotateTo(90.0f, 5f),
+//                        moveTo(300.0f, 0f, 5f)
+//                )
+//        );
+
+        // Pooling actions
+        final MyActor myActor1 = new MyActor();
+        Pool<MoveToAction> actionPool = new Pool<MoveToAction>() {
+            @Override
+            protected MoveToAction newObject() {
+                return new MoveToAction();
+            }
+        };
+
+        MoveToAction moveToAction3 = actionPool.obtain();
+        moveToAction3.setDuration(5f);
+        moveToAction3.setPosition(300f, 0);
+
+//        myActor1.addAction(moveToAction3);
+
+        // Logging actions
+        myActor1.addAction(sequence(moveToAction3,
+                run(new Runnable() {
+                    @Override
+                    public void run() {
+                        Gdx.app.log("STATUS", "Action complete");
+                    }
+                })));
+
+        stage.addActor(myActor1);
+
     }
+
 
     @Override
     public void resize(int width, int height) {
